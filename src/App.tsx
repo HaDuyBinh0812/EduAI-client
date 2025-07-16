465bed52cf87f4560cd9f32db50c747d29ec3e14
Binh <haduybinh55555@gmail.com>
2025-07-17 02:25:43 +0700

    updateg.ignore

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { useAuthStore } from "./features/auth/stores/authStore";
 
 function App() {
     const fetchCurrentUser = useAuthStore((state) => state.fetchCurrentUser);
-
+    console.log(import.meta.env.VITE_API_BASE_URL);
     useEffect(() => {
         fetchCurrentUser(); // Gọi khi app mount
     }, [fetchCurrentUser]);
--- src/layout/DefaultLayout/DefaultLayout.tsx
+++ src/layout/DefaultLayout/DefaultLayout.tsx
@@ -5,6 +5,7 @@ import styles from "./DefaultLayout.module.scss";
 import Sidebar from "@/components/SideBar";
 
 const DefaultLayout = () => {
+    console.log(import.meta.env.VITE_API_BASE_URL);
     return (
         <div className={styles.wrapper}>
             <Header />
